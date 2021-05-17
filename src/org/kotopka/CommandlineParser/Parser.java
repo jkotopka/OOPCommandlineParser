67e6a1a895b603d8e581042359a02eb6b8ff1954
John Kotopka <jkotopka@gmail.com>
2021-05-17 15:03:04 -0500

    Make Parser an interface

--- src/org/kotopka/CommandlineParser/Parser.java
+++ src/org/kotopka/CommandlineParser/Parser.java
@@ -1,93 +1,16 @@
 package org.kotopka.CommandlineParser;
 
-import java.util.*;
+import java.util.List;
 
-public class Parser {
+public interface Parser {
 
-    private int argIndex;
-    private final List<String> argList;
-    private final Map<Switch, Option> options;
-    private final Switch defaultOption;
-    private final String validDelimiter;
-
-    public Parser(String[] args, Switch defaultOption) {
-        Objects.requireNonNull(args, "args argument cannot be null");
-        Objects.requireNonNull(defaultOption, "optionSwitch argument cannot be null");
-
-        this.argList = List.of(args);
-        this.options = new LinkedHashMap<>();
-        this.defaultOption = defaultOption;
-        this.validDelimiter = Switch.getDefault();
-    }
-
-    int getArgIndex() { return argIndex; }
-
-    List<String> getArgs() { return argList; }
-
-    public Parser addOptions(Option... optionArgs) {
-        Objects.requireNonNull(optionArgs, "optionArgs argument cannot be null");
-
-        try {
-            for (Option o : optionArgs)
-                options.put(o.getSwitch(), o);
-        } catch (NullPointerException e) {
-            System.err.println("Parser initialization failed! Null option found. Please check that all Options are valid.");
-            System.exit(-1);
-        }
-
-        return this;
-    }
-
-    public Parser addOption(Option option) {
-        addOptions(option);
-
-        return this;
-    }
-
-    public void parseArgs() {
-        while (argIndex < argList.size()) {
-            String arg    = argList.get(argIndex);
-            Switch option = Switch.get(arg);
-
-            if (options.containsKey(option))
-                executeOptionAndUpdateIndex(option);
-            else if (isInvalidOption(arg))
-                printInvalidOptionMessageAndExit(arg);
-            else
-                executeOptionAndUpdateIndex(defaultOption);
-        }
-    }
-
-    private void executeOptionAndUpdateIndex(Switch option) {
-        argIndex = options.get(option).execute(this);
-    }
-
-    private boolean isInvalidOption(String arg) {
-        return arg.startsWith(validDelimiter);
-    }
-
-    private void printInvalidOptionMessageAndExit(String arg) {
-        System.err.println("Invalid option: " + arg);
-        printOptions();
-        System.exit(-1);
-    }
-
-    public Option getOption(Switch commandlineSwitch) {
-        Objects.requireNonNull(commandlineSwitch, "commandlineSwitch argument cannot be null");
-
-        return options.get(commandlineSwitch);
-    }
-
-    public void printOptions() {
-        options.forEach((key, value) -> System.out.println(value.getHelp()));
-    }
-
-    public void printValues() {
-        options.forEach((key, value) -> System.out.println(value));
-    }
-
-    public void printState() {
-        options.forEach((key, value) -> System.out.println(value.getState()));
-    }
-
+    Parser addOption(Option option);
+    Parser addOptions(Option... optionArgs);
+    int getArgIndex();
+    List<String> getArgs();
+    Option getOption(Switch commandlineSwitch);
+    void printOptions();
+    void printValues();
+    void printState();
+    void parseArgs();
 }
