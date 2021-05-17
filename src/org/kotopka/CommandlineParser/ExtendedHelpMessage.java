5ff176ff97d8e9731e1eda37036b0d7730eb4215
John Kotopka <jkotopka@gmail.com>
2021-05-17 11:39:53 -0500

    Add initial version of ExtendedHelpMessage.java

---
+++ src/org/kotopka/CommandlineParser/ExtendedHelpMessage.java
@@ -0,0 +1,48 @@
+package org.kotopka.CommandlineParser;
+
+import java.util.List;
+
+public class ExtendedHelpMessage extends AbstractOption {
+
+    private boolean value;
+
+    public ExtendedHelpMessage() {
+        this.commandlineSwitch = Switch.EXTENDED_HELP;
+        this.description = "Get extended help for option";
+    }
+
+    @Override
+    public String getState() {
+        return description + ": " + value;
+    }
+
+    @Override
+    public int execute(Parser parser) {
+        int argIndex = parser.getArgIndex();
+        List<String> args = parser.getArgs();
+
+        if (args.size() == 1) {
+            System.err.println("Usage: blah blah blah...");
+            System.exit(-1);
+        }
+
+        String arg = parser.getArgs().get(++argIndex);
+
+        value = true;
+
+        if (Switch.get(arg) != null)
+            System.out.println(arg + ": " + parser.getOption(Switch.get(arg)).getExtendedHelp());
+        else
+            System.err.println("Unknown option: " + arg);
+
+        System.exit(0);
+
+        return Integer.MAX_VALUE; // XXX: hackish workaround, never actually gets returned
+    }
+
+    @Override
+    public String toString() {
+        return commandlineSwitch + " " + value;
+    }
+
+}
