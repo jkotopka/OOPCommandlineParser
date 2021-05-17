3c63f419287805fe9731fe7e27bb1f2ef8df787b
John Kotopka <jkotopka@gmail.com>
2021-05-17 11:39:26 -0500

    Add extended help and static NEWLINE terminator

--- src/org/kotopka/CommandlineParser/AbstractOption.java
+++ src/org/kotopka/CommandlineParser/AbstractOption.java
@@ -2,12 +2,20 @@ package org.kotopka.CommandlineParser;
 
 public abstract class AbstractOption implements Option {
 
+    static final String NEWLINE = System.lineSeparator();
+
     Switch commandlineSwitch;
     String description;
+    String extendedHelpMessage = "No extended help available";
 
     @Override
     public String getHelp() {
         return String.format("\t%-5s\t%s", commandlineSwitch, description);
+    }
+
+    @Override
+    public String getExtendedHelp() {
+        return extendedHelpMessage;
     }
 
     @Override
