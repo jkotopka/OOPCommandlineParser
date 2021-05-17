dbfa8f1d054f3a32453b5a452a0eb0ca780c88cf
John Kotopka <jkotopka@gmail.com>
2021-05-17 11:38:36 -0500

    Add getExtendedHelp() to interface

--- src/org/kotopka/CommandlineParser/Option.java
+++ src/org/kotopka/CommandlineParser/Option.java
@@ -3,6 +3,7 @@ package org.kotopka.CommandlineParser;
 public interface Option {
     Switch getSwitch();
     String getHelp();
+    String getExtendedHelp();
     String getState();
     int execute(Parser parser);
     default int getInt() { return  0; }
