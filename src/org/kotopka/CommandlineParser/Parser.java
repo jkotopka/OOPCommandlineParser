3ca121da397b905f84b5ad8c5edbf8464502d0a3
John Kotopka <jkotopka@gmail.com>
2021-05-17 16:23:03 -0500

    Update parser with new methods

--- src/org/kotopka/CommandlineParser/Parser.java
+++ src/org/kotopka/CommandlineParser/Parser.java
@@ -1,16 +1,15 @@
 package org.kotopka.CommandlineParser;
 
-import java.util.List;
-
 public interface Parser {
 
     Parser addOption(Option option);
     Parser addOptions(Option... optionArgs);
-    int getArgIndex();
-    List<String> getArgs();
+    boolean hasNextArg();
+    String getNextArg();
     Option getOption(Switch commandlineSwitch);
     void printOptions();
     void printValues();
     void printState();
     void parseArgs();
+
 }
