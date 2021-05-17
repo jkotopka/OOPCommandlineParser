70c942fc6094e7d3b1561cdfa5bfe3dff644bd7b
John Kotopka <jkotopka@gmail.com>
2021-05-17 15:03:34 -0500

    Update main() to use CommandlineParser

--- src/org/kotopka/CommandlineParser/ParserTest.java
+++ src/org/kotopka/CommandlineParser/ParserTest.java
@@ -3,7 +3,7 @@ package org.kotopka.CommandlineParser;
 public class ParserTest {
 
     public static void main(String[] args) {
-        Parser parser = new Parser(args, Switch.COLLECT_PHRASE);
+        Parser parser = new CommandlineParser(args, Switch.COLLECT_PHRASE);
         parser.addOptions(
                 new DictFile(),
                 new MinWordLen(),
@@ -28,9 +28,9 @@ public class ParserTest {
 //        parser.printValues();
         parser.printState();
 
-//        System.out.println(parser.getOption(Switch.COLLECT_PHRASE).getString());
-//        System.out.println(parser.getOption(Switch.MAX_WORD_LENGTH).getInt());
-//        System.out.println(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool());
-//        System.out.println(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool());
+        System.out.println(parser.getOption(Switch.COLLECT_PHRASE).getString());
+        System.out.println(parser.getOption(Switch.MAX_WORD_LENGTH).getInt());
+        System.out.println(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool());
+        System.out.println(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool());
     }
 }
