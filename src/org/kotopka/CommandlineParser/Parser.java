5e3a53511e6bc3fb5a32f0b97f87d2f00388218e
John Kotopka <jkotopka@gmail.com>
2021-05-17 14:15:32 -0500

    Remove main() from Parser.java

--- src/org/kotopka/CommandlineParser/Parser.java
+++ src/org/kotopka/CommandlineParser/Parser.java
@@ -90,35 +90,4 @@ public class Parser {
         options.forEach((key, value) -> System.out.println(value.getState()));
     }
 
-    public static void main(String[] args) {
-        Parser parser = new Parser(args, Switch.COLLECT_PHRASE);
-        parser.addOptions(
-                new DictFile(),
-                new MinWordLen(),
-                new MaxWordLen(),
-                new ExcludeFile(),
-                new MaxResults(),
-                new Timeout(),
-                new MaxWords(),
-                new StartFrom(),
-                new IncludeWord(),
-                new ExcludeWord(),
-                new IncludeWordWithSuffix(),
-                new RestrictPermutations(),
-                new ExcludeDuplicates(),
-                new HelpMessage(),
-                new ExtendedHelpMessage(),
-                new PhraseCollector());
-
-        parser.parseArgs();
-
-//        parser.printOptions();
-        parser.printValues();
-        parser.printState();
-
-        System.out.println(parser.getOption(Switch.COLLECT_PHRASE).getString());
-        System.out.println(parser.getOption(Switch.MAX_WORD_LENGTH).getInt());
-        System.out.println(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool());
-        System.out.println(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool());
-    }
 }
